f7870e342f8613db6e85b7d389e5c4ea014d4efb
SANTIAGO EITAN SOLMESKY <48458614@ort>
2025-03-26 09:34:14 -0300

    avances en el trabajo, falta verificar que este bien

--- Index.js
+++ Index.js
@@ -3,11 +3,57 @@ function comprobarCantidadCaracteres()
     var nombre = document.getElementById("nombre");
     var errorDiv = document.getElementById("error");
 
-    if(nombre.value.length < 3) 
+
+    if(nombre.value.length < 3)
     {
         errorDiv.textContent = "Debe tener por lo menos 3 caracteres";
     }
     else{
         errorDiv.textContent = "";
     }
+}
+
+
+function validarEmail() {
+    var email = document.getElementById("mail").value;
+    var esValido = email.contains("@") && email.contains(".");
+    var mensaje = document.getElementById("mensaje");
+    if (esValido) {
+        mensaje.innerText = "Email valido";
+    } else {
+        mensaje.innerText = "Email no valido";
+       
+    }
+}
+
+
+function validarPassword()
+{
+    var password = document.getElementById("password");
+    var errorDiv = document.getElementById("error");
+
+
+    if((password.value.length < 8) && (password.contains ("") && password.contains()))
+    {
+        errorDiv.textContent = "Debe tener por lo menos 8 caracteres, y tiene que incluir 1 letra y 1 numero";
+    }
+    else{
+        errorDiv.textContent = "";
+    }
+}
+
+
+function comprobarPasswordIguales()
+{
+    var password = document.getElementById("password1");
+    var segundaPassword = document.getElementById("password2");
+    var mensaje = document.getElementById("mensaje")
+    if(password1 == password2)
+    {
+        mensaje.innerText = "";
+    }
+    else{
+        mensaje.innerText = "Las contraseñas tiene que ser iguales";
+    }
+   
 }
